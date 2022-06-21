4f08abe65a25476cfc0fb0872f0da7371202a3f5
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-06-21 16:23:09 +0600

    Project Complete :octocat:

--- chatize_project/settings.py
+++ chatize_project/settings.py
@@ -106,7 +106,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [ 
+    'chatize_project/static',
+ ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
